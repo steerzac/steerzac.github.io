57b4f2f5d84ece1f275b55473a9fbf9162a6526a
Zac <steerzac@gmail.com>
2017-06-24 15:15:06 -0700

    Deploying as Tornado App

---
+++ app.py
@@ -0,0 +1,52 @@
+#!/usr/local/bin/python3
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+import tornado.httpclient
+import tornado.websocket
+import os
+import requests
+
+
+class BaseHandler(tornado.web.RequestHandler):
+	def get_current_user(self):
+		return self.get_secure_cookie("user")
+	def get_current_email(self):
+		return self.get_secure_cookie("email")
+	def get(self):
+		self.set_header("Content-Type", "application/json")
+
+
+class IndexHandler(BaseHandler):
+	def get(self):
+		self.render("templates/html/index.html", message=0, user=self.get_current_user())
+
+
+
+
+
+
+
+settings = {
+	"login_url":"/login",
+	"compress_reponse":True,
+	"cookie_secret":"b'4rp+0kDTQ8m5wgZ7F2eRYg0NXlVoF0IYmL9Z2GrpUdA='"
+}
+
+
+def make_app():
+	return tornado.web.Application([
+		(r"/static/(.*)", tornado.web.StaticFileHandler, {
+			"path":os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+		}),
+		(r"/",IndexHandler),
+	], debug=True,compress_response=True, **settings)
+
+
+if __name__ == "__main__":
+	app = make_app()
+	http_server = tornado.httpserver.HTTPServer(app)
+	port = int(os.environ.get("PORT",5000))
+	http_server.listen(port)
+	print("Running at localhost:5000")
+	tornado.ioloop.IOLoop.current().start()
